eaf1476fd93ab72a1a698616298318621eb1d6e0
NelliShevchenko <gho020507@gmail.com>
2024-06-17 16:14:04 +0300

    brek

--- js/task-1.js
+++ js/task-1.js
@@ -61,3 +61,21 @@ do {
 for (let i = 0; i <= 20; i += 5) {
   console.log(i);
 }
+function findNumberFromFive(max, target) {
+	console.log("Log in the body of the function before the cycle");
+
+  for (let i = 5; i <= max; i += 1) {
+    console.log("Current counter value i:", i);
+
+    if (i === target) {
+      console.log(`Found the number $ {target}, interrupt the cycle`);
+			break;
+    }
+  }
+
+  console.log("Log in body function after cycle");
+}
+
+findNumber(10, 6);
+console.log("Log after exiting function");
+
