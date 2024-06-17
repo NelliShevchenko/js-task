2afd67acb3257b0f44fa5ea86ba22614ccd61559
NelliShevchenko <gho020507@gmail.com>
2024-06-17 15:37:07 +0300

    task

---
+++ js/task-1.js
@@ -0,0 +1,63 @@
+const message = "Please buy our stuff!";
+const hasSpam = message.includes("buy");
+
+if (hasSpam) {
+  console.log("Warning: This message contains forbidden words.");
+} else {
+  console.log("You can safely open this message.");
+}
+
+
+str.startsWith(substr)
+
+
+const str = "Hello, world!";
+
+console.log(str.startsWith("Hello")); // true
+console.log(str.startsWith("hello")); // false (метод чутливий до регістру)
+
+
+
+while (count < 10) {
+  console.log(`Count: ${count}`);
+  count += 1;
+}
+let clientCounter = 18;
+const maxClients = 25;
+
+while (clientCounter < maxClients) {
+  console.log(clientCounter);
+  clientCounter += 1;
+}
+function calculateTotal(number) {
+  let total = 0;
+  
+  // Проходимо через всі числа від 1 до number
+  for (let i = 1; i <= number; i++) {
+    total += i;
+  }
+  
+  return total;
+}
+
+// Приклади виклику функції
+console.log(calculateTotal(1)); // повертає 1
+console.log(calculateTotal(3)); // повертає 6 (1 + 2 + 3)
+console.log(calculateTotal(0)); // повертає 0
+console.log(calculateTotal(18)); // повертає 171 (сума чисел від 1 до 18)
+console.log(calculateTotal(24)); // повертає 300 (сума чисел від 1 до 24)
+
+do {
+   statement // код, який буде виконуватися
+} while (condition);
+
+let count = 0;
+
+do {
+	console.log(`Count: ${count}`);
+	count += 1;
+} while (count < 5);
+
+for (let i = 0; i <= 20; i += 5) {
+  console.log(i);
+}
